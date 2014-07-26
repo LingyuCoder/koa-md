91547f85cd91078c96982739b69f807ae8e682f3
天镶 <tianxiang.wly@alibaba-inc.com>
2014-07-27 01:59:47 +0800

    v0.0.1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var env = require('jsdom').env;
 var fs = require('fs');
 var hljs = require("highlight.js");
 var jsdom = require('jsdom').jsdom;
@@ -6,83 +5,123 @@ var markdown = require("markdown").markdown;
 var entities = require("entities");
 var PATH = require('path');
 
-var r_md_post = /(.md)|(.markdown)|(.mdown)$/;
-var r_deal = /(.+)\.(.+)/;
+var r_md_post = /(\.md)|(\.markdown)|(\.mdown)$/;
+
+var slice = Array.prototype.slice;
+var isArray = Array.isArray;
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+var assetPath = PATH.join(fs.realpathSync('.'), "node_modules/koa-md/assets");
+
+var defaultConfig = {
+	highlight: true,
+	highlightStyle: "github",
+	path: [],
+	index: PATH.join(assetPath, "md/index.md"),
+	error: PATH.join(assetPath, "md/error.md"),
+	customCSS: false,
+	contentOnly: true
+};
 
 hljs.configure({
 	tabReplace: '    '
 })
 
+
 module.exports = function(opts) {
-	var path = opts.path;
-	var indexFile = opts.index;
-	var style = opts.style || 'github';
-	var preTpl = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title>Document</title> <link rel="stylesheet" href="/css/' + style + '.css" /><link rel="stylesheet" href="/css/typo.css" /><link rel="stylesheet" href="/css/style.css" /></head> <body>';
-	var postTpl = '</body> </html>';
+	var config = mix({}, defaultConfig, opts);
 	return function * (next) {
 		var res = this;
-		var file = res.path;
+		var file = decodeURI(res.path);
 		var raw = res.query.raw;
 		var hasFile = true;
+		var contentOnly = config.contentOnly;
 		var ret;
-		if (path && (!r_deal.test(file) || r_md_post.test(file))) {
-			res.body = preTpl;;
-			addIndexPanel(res, path);
-
-			if (!r_md_post.test(file) && !(file = getIndexFile(file, indexFile))) {
-				this.body += '<article class="typo"><div>文件未找到</div></article>';
-			} else {
-				ret = yield readFile(file);
-				if (!raw) {
-					ret = markdown.toHTML(ret.toString());
-					this.body += '<article class="typo">' + highlight(ret) + '</article>';
-					this.body += '<a id="raw" href="' + file + '?raw=true">查看源码</a>';
-				} else {
-					this.body = ret;
-				}
-
+		if (r_md_post.test(file)) {
+			if (file === "/index.md") {
+				file = config.index;
 			}
-			res.body += postTpl;
+			if (!fs.existsSync(file)) {
+				file = config.error;
+			}
+			ret = yield readFile(file);
+			res.body = res.body || "";
+			if (!raw) {
+				if (!contentOnly) {
+					res.body += yield createHeader(file, config.customCSS);
+					res.body += addIndexPanel(config.path);
+				}
+				ret = markdown.toHTML(ret.toString());
+				if (config.highlight) {
+					res.body += '<style type="text/css">';
+					res.body += yield readFile(PATH.join(assetPath, 'css/' + config.highlightStyle + '.css'));
+					res.body += '</style>';
+				}
+				res.body += '<article class="typo">' + (config.highlight ? highlight(ret) : ret) + '</article>';
+				res.body += '<a id="raw" href="' + file + '?raw=true">查看源码</a>';
+				if (!contentOnly) {
+					res.body += createFooter();
+				}
+			} else {
+				res.body += ret;
+			}
 			res.set('Last-Modified', (new Date).toUTCString());
 			res.set('Cache-Control', 'max-age=0');
-			res.set('Content-Type', 'text/' + (raw ? 'plain':'html') + ' ; charset=utf-8');
+			res.set('Content-Type', 'text/' + (raw ? 'plain' : 'html') + ' ; charset=utf-8');
+
 		}
 		yield next;
 	};
 };
 
-function getIndexFile(category, indexFile) {
-	if (category === '/' && !indexFile) {
-		return "node_modules/koa-md/readme.md";
-	} else {
-		return indexFile;
-	}
-	var postTest = ['markdown', 'mdown', 'md'];
-	var i;
-	for (i = postTest.length; i--;) {
-		var path = PATH.join(category, "/index." + postTest[i]);
-		if (fs.existsSync(path)) {
-			return path;
+function createHeader(name, customCSS) {
+	return function * (done) {
+		var tpl = '<!DOCTYPE html> <html> <head> <meta charset="UTF-8"> <title>' + name + '</title>';
+		if (customCSS && isArray(customCSS)) {
+			customCSS.forEach(function(css) {
+				tpl += '<link rel="stylesheet" href="' + css + '" />';
+			});
+			tpl += '</head><body>';
+			return tpl;
+		} else {
+			tpl += '<style type="text/css">';
+			tpl += yield readFile(PATH.join(assetPath, "css/typo.css"));
+			tpl += '</style>';
+			tpl += '<style type="text/css">';
+			tpl += yield readFile(PATH.join(assetPath, "css/style.css"));
+			tpl += '</style></head><body>';
+			return tpl;
 		}
-	}
-	return;
+	};
 }
 
-function addIndexPanel(res, path) {
+function createFooter() {
+	return '</body></html>';
+}
+
+function addIndexPanel(path) {
+	var result = "";
 	path = toArray(path);
-	res.body += '<div id="panel">';
+	result += '<div id="panel">';
 	path.forEach(function(p) {
 		var dir = p.dir || "";
 		var label = p.label || p.dir;
 		var fileList = walk(dir, "");
-		res.body += '<p><a href="' + dir + '">' + label + '</a></p>';
-		res.body += '<ul>';
+		if(fs.existsSync(PATH.join(dir, "index.md"))){
+			result += '<p><a href="' + PATH.join(dir, "index.md") + '">' + label + '</a></p>';	
+		} else {
+			result += '<p>' + label + '</p>';
+		}
+		
+		result += '<ul>';
 		fileList.forEach(function(file) {
-			res.body += '<li><a href="' + PATH.join(dir, file) + '">' + file + '</a></li>';
+			if (file !== "index.md") {
+				result += '<li><a href="' + PATH.join(dir, file) + '">' + file + '</a></li>';
+			}
 		});
-		res.body += '</ul>';
+		result += '</ul>';
 	});
-	res.body += '</div>'
+	result += '</div>'
+	return result;
 }
 
 function readFile(path) {
@@ -124,6 +163,18 @@ function walk(path, base) {
 		}
 	});
 	return list;
+}
+
+function mix(dest) {
+	var srcs = slice.call(arguments, 1);
+	srcs.forEach(function(src) {
+		for (var item in src) {
+			if (hasOwnProperty.call(src, item)) {
+				dest[item] = src[item];
+			}
+		}
+	});
+	return dest;
 }
 
 function toArray(obj) {
